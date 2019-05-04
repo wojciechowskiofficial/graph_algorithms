5b74ddf0051705aa3d8f04c47d94b5388aeb3347
ADAM WOJCIECHOWSKI <adi61044@gmail.com>
2019-05-04 17:23:38 +0200

    random index handling fill utility method added

--- Utility.h
+++ Utility.h
@@ -1,11 +1,30 @@
 class Utility {
 	public:
 		void shuffle(int * a, int s);
+		void fill(int * a, int s, bool flag);
 };
 
 void Utility::shuffle(int * a, int s) {
 	srand(time(NULL));
 	for (int i = s - 1; i > 0; i--) {
 		std::swap(a[i], a[rand() % (i + 1)]);
+	}
+}
+
+void Utility::fill(int * a, int squared, bool flag) {
+	int counter = 0;
+	if (flag) {
+		for (int i = 0; i < squared; i++) {
+			for (int j = 0; j < squared; j++) {
+				a[counter++] = j;
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < squared; i++) {
+			for (int j = 0; j < squared; j++) {
+				a[counter++] = i;
+			}
+		}
 	}
 }
